30b2c3e1034de9377a0f673b85134af10fe0aaba
Tom Stroo <tomstroo3@gmail.com>
2023-03-31 16:46:16 +0200

    fixed order

--- test2.py
+++ test2.py
@@ -4,8 +4,8 @@ def testfunc(x, y):
 def testfunc2(a, b, c):
 	return a, b, c
 
-def testfunc4(fruit):
-	return f"Fruit {fruit}"
-
 def testfunc3(*args):
 	return args
+	
+def testfunc4(fruit):
+	return f"Fruit {fruit}"
